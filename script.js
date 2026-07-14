3e641c30d546405fdfaf6333e1dfa9138cda86e6
reelaiuz <axbokiev@gmail.com>
2026-07-14 11:13:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { auth, provider, signInWithPopup } from "./firebase.js";
 const btn = document.getElementById("createBtn");
 
 btn.onclick = function(){
@@ -11,5 +12,28 @@ if(text==""){
 
 document.getElementById("result").innerHTML =
 "<h3>✅ So'rov qabul qilindi</h3><p>Keyingi bosqichda internetdan mos videolar izlanadi.</p>";
+
+}
+
+const googleBtn = document.getElementById("googleLogin");
+
+googleBtn.onclick = async function () {
+
+    try {
+
+        const result = await signInWithPopup(auth, provider);
+
+        document.getElementById("result").innerHTML =
+        "<h3>✅ Xush kelibsiz!</h3><p>" +
+        result.user.displayName +
+        "<br>" +
+        result.user.email +
+        "</p>";
+
+    } catch (error) {
+
+        alert("Google Login xatosi: " + error.message);
+
+    }
 
 }
